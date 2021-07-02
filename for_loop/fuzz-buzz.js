6fedb037c392ba857d0399d1fde9f4fde97a977b
aabhishek-chaurasia-au17 <abhishekc838@gmail.com>
2021-07-02 13:53:34 +0530

    pr

---
+++ for_loop/fuzz-buzz.js
@@ -0,0 +1,14 @@
+// Write a JavaScript program which iterates the integers from 1 to 50. For divisible of three print "Fizz" instead of the number and for the divisible of five print "Buzz". For numbers which are divisible of both three and five print "FizzBuzz".
+
+
+
+
+for(var i = 1; i < 51; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz")
+    }else if(i % 5 === 0){
+        console.log("Buzz")
+    }else if(i % 3 === 0){
+        console.log("Fizz")
+    }
+}
--- for_loop/reverse_loop.js
+++ for_loop/reverse_loop.js
@@ -1,9 +1,63 @@
 
-let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
-// Example output:
-// true 3.5 be false cannot true 9 what 43 OR each item on a new line
+// let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+// // Example output:
+// // true 3.5 be false cannot true 9 what 43 OR each item on a new line
 
-for(let i = arr.length -1; i >= 0; i--){
-    console.log(arr[i]);
-}
+
+let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+
+// for(let i = arr.length -1; i >= 0; i--){
+//     console.log(arr[i]);
+// }
+
+let array = arr.reverse();
+console.log(array)
+
+// const num = [,2,3,4,5];
+// var target = 5;
+// for(let j = 0; j < num.length; j++){
+//     if(num[j] === target){
+
+//        console.log("yes");
+//         break;
+//     }else if(num[j] !== target){
+//         console.log("no");
+//         continue;
+//     }
+// }
+
+
+// const num = [1,2,3,4,5]
+// target = 5;
+
+
+// for(let j = 0; j < num.length; j++){
+//     if(num[j] === target){
+
+//        console.log("yes");
+//         break;
+//     }else if(num[j] !== target){
+//         continue;
+//     }
+// }
+
+
+// for(i in num){
+//     if(i == target){
+//         console.log('yes');
+        
+//     }else{
+//         console.log(-1);
+//     }
+// }
+
+// num.forEach(a => {
+//     if (a == target){
+//         console.log("yes");
+        
+//     }else{
+//         console.log("no");
+//     }
+    
+// });
 
