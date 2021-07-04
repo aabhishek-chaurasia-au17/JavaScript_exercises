6fee1d8201a9c0fb79c0bc3cf62f7e3a62ba2a17
aabhishek-chaurasia-au17 <abhishekc838@gmail.com>
2021-07-04 06:29:19 +0530

    morning

--- geekQues/findlargestnum.js
+++ geekQues/findlargestnum.js
@@ -2,15 +2,23 @@
 // JavaScript Program to find largest element in an array
 
 
+let arr = [20, 10, 20, 4, 100, 10, 50]
+
 // my way 1
 
-let arr = [20, 10, 20, 4, 100]
-let max = arr[0]
-const largest = () =>{
-    for(let i = 0; i < arr.length; i++)
-    if ( arr[i] > max){
-        max = arr[i]
-        return max
-    }
-}
-console.log(largest());
+// let max = arr[0]
+// const largest = () =>{
+//     for(let i = 0; i < arr.length; i++)
+//     if ( arr[i] > max){
+//         max = arr[i]
+//         return max
+//     }
+// }
+// console.log(largest());
+
+
+// Way 2
+
+var maxi = Math.max(...arr);
+
+console.log(maxi);
