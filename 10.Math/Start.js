99f87e9e3d14d2159708b6ec922fb39d47918d47
aabhishek-chaurasia-au17 <abhishekc838@gmail.com>
2021-07-05 06:25:51 +0530

    practice

---
+++ 10.Math/Start.js
@@ -0,0 +1,50 @@
+// The JavaScript Math Object Allows you to perform mathematical tasks on Number.
+
+console.log(Math.PI);
+// Math.round()
+
+let num = 10.2565;
+console.log(Math.round(num));
+
+// math.pow
+
+console.log(Math.pow(5,5));
+
+// Math.Sqrt()
+
+console.log(Math.sqrt(25));
+console.log(Math.sqrt(81));
+
+// math.abs()
+
+console.log(Math.abs(-55));
+console.log(Math.abs(4-6));
+
+
+// Math.ceil()
+
+console.log(Math.ceil(4.4));
+console.log(Math.ceil(99.4));
+
+// Math.min()
+
+console.log(Math.min(0,150,30,20,-8,200));
+
+// Math.Max()
+
+console.log(Math.max(0,150,30,20,-8,200));
+
+
+// Math.rondom()
+
+console.log(Math.round()*10);
+
+// Math.trunc()
+
+console.log(Math.trunc(4.6));
+console.log(Math.trunc(-99.6));
+
+
+
+
+
--- 9.Date&time/Start.js
+++ 9.Date&time/Start.js
@@ -11,4 +11,71 @@ const today = new Date();
 console.log(today);
 
 
+console.log(new Date());
+
+console.log(new Date().toLocaleString());
+
+console.log(new Date().toString())
+
+// Date.now()
+
+console.log(Date.now());
+
+var d = new Date(2021,0,24,10,33,30,0)
+
+console.log(d.toLocaleString());
+
+// new Date(date String)
+
+// var d = newDate (October 13, 2021, 11:13:00);
+// console.log(d.toLocaleString());
+
+// newDate(milliseconds)
+var d = new Date(0);
+console.log(d);
+
+// Dates Method
+const curDate = new Date();
+
+// how to get the indivisual date
+
+console.log(curDate.toLocaleString());
+console.log(curDate.getFullYear());
+console.log(curDate.getMonth());
+console.log(curDate.getDate());
+console.log(curDate.getDay());
+
+
+// Time Method
+
+const curtime = new Date();
+console.log(curtime.getTime());
+
+console.log(curtime.getHours());
+
+console.log(curtime.getUTCMinutes());
+ 
+console.log(curtime.getUTCSeconds());
+
+console.log(curtime.getUTCMilliseconds());
+
+// how to set the Indivisual time.
+
+console.log(curtime.setTime());
+console.log(curtime.setTime(5));
+console.log(curtime.setMinutes(5));
+console.log(curtime.setSeconds(5));
+console.log(curtime.setMilliseconds(5));
+
+
+
+
+
+
+
+
+
+
+
+
 
