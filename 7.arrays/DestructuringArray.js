de7a9d0d0180aa205ff4a51d884467e1f984ace1
aabhishek-chaurasia-au17 <abhishekc838@gmail.com>
2021-07-12 13:43:21 +0530

    practice

---
+++ 7.arrays/DestructuringArray.js
@@ -0,0 +1,27 @@
+
+// Destructuring in JavaScript ES6 ?
+// The destructuring assignment syntex is JavaScript expression that make it  possible to unpack values from Arrays, or properties from object, into distinct variables.
+
+// Array Destructuring
+
+// const myBioData = ['Abhishek', 'Chaurasia', 26];
+// // old Vartion
+// let myFName = myBioData[0];
+// let myLName = myBioData[1];
+// let myAge = myBioData[2];
+
+// console.log(myFName);
+
+// New Vartion
+// let [myFName, myLName, myAge] = mydata;
+
+// console.log(myAge);
+
+// let [myFName, myLName, myAge,] = mydata;
+
+// console.log(myLName);
+
+let txt = "I can eat bananas all day";
+let x = txt.slice(10,17);
+
+console.log(x);
