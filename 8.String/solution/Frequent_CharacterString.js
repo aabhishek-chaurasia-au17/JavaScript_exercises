3261b9104c039a8818edf3a5d0dff6b3cded6e0f
aabhishek-chaurasia-au17 <abhishekc838@gmail.com>
2021-07-18 05:08:45 +0530

    hare krishna

---
+++ 8.String/solution/Frequent_CharacterString.js
@@ -0,0 +1,17 @@
+// JavaScript Least Frequent Character in String
+
+const Least_Frequent = (str) =>{
+    let mostLength = 0;
+    let CharacterMostlenth;
+    for(let t = 0; t < str.length; t++){
+        let res = str.length - str.replece(new RegExp(t,'g'),'').length;
+        if(res > mostLength){
+            CharacterMostlenth = t;
+            mostLength = res;
+        }
+    }
+}
+
+let str = "GeeksforGeeks";
+console.log(Least_Frequent(str));
+
