4bf168a29ae0ae236bb00f1cd2df7ea1e6a586e0
aabhishek-chaurasia-au17 <abhishekc838@gmail.com>
2021-07-23 06:45:30 +0530

    linear

--- 13.Searching/linear.js
+++ 13.Searching/linear.js
@@ -1,16 +1,29 @@
 // #  Linear Search
 
-let arr = [11, 10, 15, 20, 5]
-let target = 20
+// let arr = [11, 10, 15, 20, 5]
+// let target = 20
 
-const getSearch = (arr,target) =>{
-    for(let i = 0; i < arr.length; i++){
-         if(arr[i] === target){
-             return i
-         }
-     }return false
+// const getSearch = (arr,target) =>{
+//     for(let i = 0; i < arr.length; i++){
+//          if(arr[i] === target){
+//              return i
+//          }
+//      }return false
+// }
+// console.log(getSearch(arr,target)); 
+
+
+
+
+let fruits = ['apple','mango','Kivi', 'bananaa', 'avacrdo']
+let x = 'mango'
+
+let getseach = (fruits,x) => {
+    for(let j =0; j< fruits.length; j++){
+        if(fruits[j] === x){
+            console.log(j);
+        }
+    }
 }
-console.log(getSearch(arr,target)); 
 
-
-
+getseach(fruits,x)
