945e88b32098944a64f0c8b47cee4cc2f459d5d7
aabhishek-chaurasia-au17 <abhishekc838@gmail.com>
2021-07-26 06:30:00 +0530

    binery search

--- 13.Searching/binery-search.js
+++ 13.Searching/binery-search.js
@@ -1,13 +1,23 @@
 // binery-search
 
 let array = [1,3,4,6,8,10,14];
-const x = 5;
+const x = 3;
 
 let getseaching = (array,x) => {
-    for(let i = 0; i < array.length; i++){
-        if(array[i]=== x){
-            return true
+    let left = 0;
+    let right = array.length-1;
+
+    while (left <= right){
+        let mid = Math.floor((left+right)/2);
+        if(array[mid]==x){
+            return true ;
+        }else if (array[mid] < x){
+            left = mid + 1;
+        }else{
+            right = mid -1;
         }
-    }return -1
+        
+    }return false;
+
 }
 console.log(getseaching(array,x));
