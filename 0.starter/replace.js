ba14d1ab8b6d6465da1e6539ff2a47ea398169b8
Abhishek <abhishekc838@gmail.com>
2021-08-03 05:09:00 +0530

    JS practice

---
+++ 0.starter/replace.js
@@ -0,0 +1,12 @@
+
+// replece One by one
+
+let str = "abhishek chaurasia";
+
+console.log(str.replace('a','d'));
+
+// replece all in one time
+
+let abhi = "abhishek chaurasia";
+
+console.log(abhi.replaceAll('a','d'));
