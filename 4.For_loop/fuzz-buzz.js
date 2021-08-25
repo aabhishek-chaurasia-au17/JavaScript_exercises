2210e37f1d615664d34e68353b0a1f4d07e80a70
Abhishek <abhishekc838@gmail.com>
2021-08-25 15:15:39 +0530

    js

--- 4.For_loop/fuzz-buzz.js
+++ 4.For_loop/fuzz-buzz.js
@@ -3,12 +3,18 @@
 
 
 
-for(var i = 1; i < 51; i++){
+for(var i = 1; i < 100; i++){
+
     if(i % 3 === 0 && i % 5 === 0){
+        
         console.log("FizzBuzz")
+
     }else if(i % 5 === 0){
+
         console.log("Buzz")
+
     }else if(i % 3 === 0){
+
         console.log("Fizz")
     }
 }
