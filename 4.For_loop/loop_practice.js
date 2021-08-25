51571fe0f0a17fe369abe42aeb3d18bb5f3e5775
Abhishek <abhishekc838@gmail.com>
2021-08-25 15:13:27 +0530

    loops

--- 4.For_loop/loop_practice.js
+++ 4.For_loop/loop_practice.js
@@ -7,16 +7,22 @@ for(i = 1; i <= 10; i++){
 }
 
 
+// How a JavaScript forEach-Loop Looks Like
+
 let arr = ["Ricciardo", "Verstappen", "Bottas"];
-
-// for(let i = 0; i <= arr.length; i++){
-//     console.log(arr[i]);
-// }
-
-// for(var j in arr){
-//     console.log(arr[j]);
-// }
 
 arr.forEach((a,idx,array,) => {
     console.log(`${a} ${idx} ${arr}`);
 });
+
+
+// How a JavaScript inedx Loop Looks Like
+
+for(let i = 0; i <= arr.length; i++){
+    console.log(arr[i]);
+}
+
+// How a JavaScript in Loop Looks Like
+for(var j in arr){
+    console.log(arr[j]);
+}
